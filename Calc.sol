57632d7aebedd8f174f468b8e0680d0f1c9e9b2f
hephka <franckthevenin95@gmail.com>
2020-10-14 18:00:00 +0200

    natspec

--- Calc.sol
+++ Calc.sol
@@ -2,41 +2,45 @@
 pragma solidity >= 0.6.0;                       // Version pragma
 pragma experimental ABIEncoderV2;               // Experimental pragma
 
-/** @title Calculatrice simple. */
+/** @title Calculatrice.
+  * @author Franck Thevenin as Hephka.
+  * @notice Vous pouvez additionner, soustraire, multiplier et diviser deux nombres <uint>.
+  * @dev Ces fonctions ne coûtent pas de ressources et retourne le résultat de l'opération demandé seulement pour l'utilisateur ayant déployé ce contrat.
+  */
 contract Calc {
-    address private owner;
+    address private owner;                      // Déclaration de owner <adress>
 
     constructor() public {
         owner = msg.sender;
     }
 
     /** @dev Fonction permettant l'addition de 2 nombres. 
-      * nb1 Premier nombre. Type: uint.
-      * nb2 Second nombre. Type: uint.
+      * @param nb1 Premier nombre. <uint>.
+      * @param nb2 Second nombre. <uint>.
       */
     function add(uint nb1, uint nb2) public view returns (uint res) {
         require(owner == msg.sender, 'Only owner can use this function.');
         res = nb1 + nb2;
     }
     /** @dev Fonction permettant l'addition de 2 nombres. 
-      * nb1 Premier nombre. Type: uint.
-      * nb2 Second nombre. Type: uint.
+      * @param nb1 Premier nombre. <uint>.
+      * @param nb2 Second nombre. <uint>.
       */
     function sub(uint nb1, uint nb2) public view returns (uint res) {
         require(owner == msg.sender, 'Only owner can use this function.');
         res = nb1 - nb2;
     }
     /** @dev Fonction permettant l'addition de 2 nombres. 
-      * nb1 Premier nombre. Type: uint.
-      * nb2 Second nombre. Type: uint.
+      * @param nb1 Premier nombre. <uint>.
+      * @param nb2 Second nombre. <uint>.
       */
     function mul(uint nb1, uint nb2) public view returns (uint res) {
         require(owner == msg.sender, 'Only owner can use this function.');
         res = nb1 * nb2;
     }
     /** @dev Fonction permettant l'addition de 2 nombres. 
-      * nb1 Premier nombre. Type: uint.
-      * nb2 Second nombre. Type: uint.
+      * @param nb1 Premier nombre. <uint>.
+      * @param nb2 Second nombre. <uint>.
       */
     function div(uint nb1, uint nb2) public view returns (uint res) {
         require(owner == msg.sender, 'Only owner can use this function.');
